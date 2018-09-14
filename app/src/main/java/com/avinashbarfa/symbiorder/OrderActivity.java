4e0f6521eadd4ae27155672d25df32002dab373a
Avinash Barfa <avibarfa98@gmail.com>
2018-09-14 10:41:36 +0530

    Fixed Bugs while placing orders

--- app/src/main/java/com/avinashbarfa/symbiorder/OrderActivity.java
+++ app/src/main/java/com/avinashbarfa/symbiorder/OrderActivity.java
@@ -1,9 +1,10 @@
 package com.avinashbarfa.symbiorder;
 
-import android.media.MediaCas;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,13 +15,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.avinashbarfa.symbiorder.Adapters.RestaurantAdapter;
-import com.avinashbarfa.symbiorder.DataBean.RestaurantData;
 import com.avinashbarfa.symbiorder.DataBean.UrlLink;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,8 +26,10 @@ public class OrderActivity extends AppCompatActivity{
     EditText edit_username;
     EditText edit_contact;
     EditText edit_address;
-    EditText btn_place_order;
+    Button btn_place_order;
     UrlLink urlLink = new UrlLink();
+    String itemList,address,personName,contactNumber,restaurantID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,44 +39,45 @@ public class OrderActivity extends AppCompatActivity{
         edit_username = (EditText) findViewById(R.id.edit_username);
         edit_contact = (EditText) findViewById(R.id.edit_address);
 
-        String message = getIntent().getStringExtra("restaurant_id");
-        Log.v("Message", message);
+        restaurantID = getIntent().getStringExtra("restaurant_id");
+        contactNumber = edit_contact.getText().toString();
+        itemList = items_list.getText().toString();
+        address = edit_address.getText().toString();
+        personName = edit_username.getText().toString();
+        btn_place_order = (Button) findViewById(R.id.btn_place_order);
+        btn_place_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                placeOrder();
+            }
+        });
+    }
 
-        String itemList = items_list.getText().toString();
-        String address = edit_address.getText().toString();
-        String personName = edit_username.getText().toString();
-
-
+    public void placeOrder() {
         StringRequest stringRequest = new StringRequest(Request.Method.POST, urlLink.getRetriveRestaurantURL() , new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    JSONArray array = jsonObject.getJSONArray("result");
-
-                    for(int i =0; i<array.length();i++){
-                        JSONObject object = array.getJSONObject(i);
-
-
-                    }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                Toast.makeText(getApplicationContext(), response , Toast.LENGTH_LONG).show();
+                startActivity(new Intent(OrderActivity.this, MyOrderActivity.class));
+                finish();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.getMessage() , Toast.LENGTH_LONG).show();
                 }
             }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage() , Toast.LENGTH_LONG).show();
-                    }
-                }
         )
         {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
-                //params.put("category_id" , category_id);
+                params.put("items_list" , itemList);
+                params.put("address" , address);
+                params.put("personName", personName);
+                params.put("contact" , contactNumber);
+                params.put("restaurant", restaurantID);
+                params.put("status", "0");
                 return params;
             }
         };
